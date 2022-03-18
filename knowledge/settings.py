dda14d730bb4f12fee5b5c04c52e923feea97b9a
Hyeonil Na <sjrnfl528@gmail.com>
2022-03-18 12:52:33 +0900

    setting DB설정

--- knowledge/settings.py
+++ knowledge/settings.py
@@ -79,12 +79,16 @@ WSGI_APPLICATION = 'knowledge.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'knowledge',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
