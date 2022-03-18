ca02852630d92ef274049aa22cdf2150df9cd8e8
Hyeonil Na <sjrnfl528@gmail.com>
2022-03-18 12:57:26 +0900

    user serializers 수정

--- user/serializers.py
+++ user/serializers.py
@@ -7,10 +7,9 @@ from rest_framework_simplejwt.serializers import (
     TokenRefreshSerializer as OriginTokenRefreshSerializer,
 )
 
-from cart.serializers import CartSerializer
-from clothes.serializers import ClothesSerializer
+
 from notice.serializers import NoticeSerializer
-from payment.serializers import PaymentSerializer
+
 
 User = get_user_model()
 
@@ -23,9 +22,8 @@ class UserCreationSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ["user_id", "password", "password2",  "username",
-                  "user_nickname", "user_tel", "user_birth", "user_genre",
-                  "user_auth", "user_type"]
+        fields = ["user_id", "password", "password2", "name",
+                  "nickname", "phone_num", "email"]
 
     def validate(self, attrs):
         if attrs["password"] != attrs["password2"]:
@@ -34,16 +32,12 @@ class UserCreationSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         user_id = validated_data["user_id"]
-        username = validated_data["username"]
+        name = validated_data["name"]
         password = validated_data["password"]
-        user_nickname = validated_data.get("user_nickname", "")
-        user_birth = validated_data["user_birth"]
-        user_tel = validated_data["user_tel"]
-        user_genre = validated_data.get("user_genre", "")
-
-
-        new_user = User(user_id=user_id, username=username, user_nickname=user_nickname, user_tel=user_tel, user_birth=user_birth,
-                        user_genre=user_genre)
+        nickname = validated_data.get("nickname", "")
+        phone_num = validated_data["phone_num"]
+        email = validated_data["email"]
+        new_user = User(user_id=user_id, username=name, nickname=nickname, phone_num=phone_num, email=email,)
         new_user.set_password(password)
         new_user.save()
         return new_user
@@ -53,8 +47,8 @@ class TokenObtainPairSerializer(OriginTokenObtainPairSerializer):
     def validate(self, attrs):
         data: Dict = super().validate(attrs)
         data["user_id"] = self.user.user_id
-        data["username"] = self.user.username
-        data["user_nickname"] = self.user.user_nickname
+        data["name"] = self.user.username
+        data["nickname"] = self.user.user_nickname
         data["is_staff"] = self.user.is_staff
         data["is_superuser"] = self.user.is_superuser
         return data
@@ -69,5 +63,5 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ["user_id", "is_superuser","is_staff", "username","user_nickname",
-                  "user_birth","user_tel", "user_genre","notice_set", "is_active"]
+        fields = ["user_id", "is_superuser", "is_staff", "name", "nickname",
+                  "phone_num", "notice_set", "is_active"]
